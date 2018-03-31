b17561c287d9db6aaa22f1f8dae27d8335ee6510
Ingmar Drewing <ingmar@drewing.de>
2018-04-01 00:54:20 +0200

    Refactor

--- main_test.go
+++ main_test.go
@@ -44,10 +44,8 @@ func setup() {
 }
 
 func tearDown() {
-	/*
-		filepath := path.Join(getTestFileDirPath(), conf[0].Deploy.TargetDir)
-		fs.RemoveDirContents(filepath)
-	*/
+	filepath := path.Join(getTestFileDirPath(), conf[0].Deploy.TargetDir)
+	fs.RemoveDirContents(filepath)
 }
 
 func getTestFileDirPath() string {
